581cefc57837eaf0f1ee87287df78883b24eef20
unknown <mdshahalammia520@gmail.com>
2022-11-01 22:47:42 +0600

    first commit

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-console.log("I love javaScript");
+let poorCountry=['Bangladesh', 'Sri lanka'];
+let richCountry=[...poorCountry,'China', 'Canada', 'America']; //separed operator
+console.log(richCountry);
+
+function calculation(...numbers){
+    let sum=0;
+    for(let i of numbers){
+        sum=sum+i;
+    }
+console.log(sum);
+
+}
+
+calculation(10,20,30,40,5,8,);
+
+function sleep(name, time ){
+     console.log(name +" sleeping time " + time); //function dynamic
+}
+sleep("Rahim", "10pm");
+sleep("Karim", "11pm");
+sleep("Meena", "12pm");
