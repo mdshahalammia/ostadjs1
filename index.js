1bb90b40d12f8505caa339f9ded3c84b5e499b4f
unknown <mdshahalammia520@gmail.com>
2022-11-04 01:30:06 +0600

    updated

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hello world");
+var studentName="Md Shah Alam Mia";
+var studentAge=23;
+var studentSchoolName="Dhaka High School";
+var isStudent=true;
+console.log("Student Details===>",studentName,studentSchoolName,studentAge,"He is my student===>",isStudent,);
+
+// 2ta string k + kora 
+
+var firstName="Md Shah";
+var lastName="Alam Mia";
+var fullName=firstName+" "+lastName;
+console.log(fullName);
